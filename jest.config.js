886da79ba7bb9016cc07c813fc8697eb910bbfda
Trung Truong <trung.truong@employmenthero.com>
2023-09-26 23:12:13 +0700

    add coverage scope

--- jest.config.js
+++ jest.config.js
@@ -10,6 +10,7 @@ const customJestConfig = {
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   testEnvironment: 'jest-environment-jsdom',
   moduleDirectories: ['node_modules', '<rootDir>/'],
+  collectCoverageFrom: ['<rootDir>/**/*.{js,jsx}']
 }
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
